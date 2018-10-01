477006fcfdd9433cf73f941c320cfa3eb388127f
Seungyoun Yi <younatics@gmail.com>
2018-10-01 19:29:49 +0900

    Initial Commit

---
+++ Triangulation/Triangulation.h
@@ -0,0 +1,19 @@
+//
+//  Triangulation.h
+//  Triangulation
+//
+//  Created by Seungyoun Yi on 01/10/2018.
+//  Copyright © 2018 Seungyoun Yi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Triangulation.
+FOUNDATION_EXPORT double TriangulationVersionNumber;
+
+//! Project version string for Triangulation.
+FOUNDATION_EXPORT const unsigned char TriangulationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Triangulation/PublicHeader.h>
+
+
